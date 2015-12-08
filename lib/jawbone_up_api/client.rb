7676b70c0a88cdd66743327bb3d2a51f378bc1e7
yimajo <y.imajo@gmail.com>
2015-12-08 20:28:38 +0900

    Intial commit.

---
+++ lib/jawbone_up_api/client.rb
@@ -0,0 +1,39 @@
+require 'jawbone_up_api/client/version'
+require 'faraday'
+require 'json'
+require 'hashie'
+
+module JawboneUPAPI
+  class Client
+    API_VERSION = '1.1'
+    BASE_URL  = "https://jawbone.com/nudge/api/v.#{API_VERSION}"
+
+    def initialize(token)
+      @token = token
+
+      @conn = Faraday.new() do |faraday|
+        faraday.request  :url_encoded
+        faraday.response :logger
+        faraday.adapter  Faraday.default_adapter
+      end
+
+      @conn.authorization(:Bearer, @token)
+    end
+
+    def sleeps(date = nil)
+      if date != nil
+        param = {date: date}
+      end
+      json = get_helper('users/@me/sleeps', param: param)
+      Hashie::Mash.new(json)
+    end
+
+    def get_helper(path, param: nil)
+      response = @conn.get("#{BASE_URL}/#{path}", param)
+
+      if response.body != nil
+        JSON.parse(response.body)
+      end
+    end
+  end
+end
---
+++ test/test_jawbone_up_api/client.rb
@@ -0,0 +1,63 @@
+require './test/minitest_helper'
+
+module TestJawboneUPAPI
+  class Client < Minitest::Test
+    def setup
+      @invalid_token = 'dummy'
+      @access_token = ''
+
+    end
+
+    def test_that_it_has_a_version_number
+      refute_nil ::JawboneUPAPI::Client::VERSION
+    end
+
+    def test_unauthorized
+      client = JawboneUPAPI::Client.new(@invalid_token)
+      sleeps = client.sleeps
+
+      assert_equal 401, sleeps.meta.code
+    end
+
+    def test_sleeps_list
+      client = JawboneUPAPI::Client.new(@access_token)
+      sleeps = client.sleeps
+
+      refute_nil sleeps.meta
+      assert_equal 'OK', sleeps.meta.message
+      assert_equal 200, sleeps.meta.code
+
+      refute_nil sleeps.data
+      refute_nil sleeps.data.items
+
+      items = sleeps.data.items
+      if 0 < items.count
+        for item in items do
+          refute_nil item.xid
+          refute_nil item.date
+        end
+      end
+    end
+
+    def test_sleep_date
+      client = JawboneUPAPI::Client.new(@access_token)
+      sleeps = client.sleeps('20151207')
+
+      refute_nil sleeps.meta
+      assert_equal 'OK', sleeps.meta.message
+      assert_equal 200, sleeps.meta.code
+
+      refute_nil sleeps.data
+      refute_nil sleeps.data.items
+
+      items = sleeps.data.items
+      if 0 < items.count
+        for item in items do
+          refute_nil item.xid
+          refute_nil item.date
+        end
+      end
+
+    end
+  end
+end
